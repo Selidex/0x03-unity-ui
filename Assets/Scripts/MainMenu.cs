97c4509429c2d2cf457ed2d0824805e8db3a647a
Dexter Selina Parnell <justindparnell@yahoo.com>
2021-08-11 10:01:01 -0500

    Uploading task 10 files (quit button)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,4 +10,9 @@ public class MainMenu : MonoBehaviour
     public void PlayMaze(){
         SceneManager.LoadScene(1);
     }
+
+    public void QuitMaze(){
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
 }
